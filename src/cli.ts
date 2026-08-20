e38bde50721d0c857ba2084f79f6b2c70ebcf9df
Yi-Ru Lin <deduce@gmail.com>
2026-08-20 13:44:53 -0500

    chore(release): 0.1.1
    Ship SSH host-key verification (TOFU pinning) patch.

--- src/cli.ts
+++ src/cli.ts
@@ -12,7 +12,7 @@ import { sshCommand } from "./commands/ssh.ts";
 import { ConfigError, loadConfig } from "./config.ts";
 import { SiteResolutionError } from "./resolve.ts";
 
-const VERSION = "0.1.0";
+const VERSION = "0.1.1";
 
 function createClient(): KinstaClient {
   return new KinstaClient(loadConfig());
